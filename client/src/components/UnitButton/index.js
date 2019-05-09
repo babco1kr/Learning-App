02421fd3a6f1c03addbc79c7ef9117a2b43a573a
Kyle Babcock <kylerbabcock@gmail.com>
2019-05-09 17:51:31 -0400

    photo link swapped with X of check if it exists

--- client/src/components/UnitButton/index.js
+++ client/src/components/UnitButton/index.js
@@ -9,10 +9,10 @@ function UnitButton(props) {
             <button className = "btn" onClick={() => props.selectUnit(props.id)}>{props.name}</button>
            </td>
            <td>
-            <button className = "waves-effect waves-light btn red" onClick={() => props.changeActive(props.id)}>InActive</button>
+            <button className = "waves-effect waves-light btn grey darken-4" onClick={() => props.changeActive(props.id)}>InActive</button>
             </td>
             <td>
-            <button className = "btn" onClick={() => props.removeUnit(props.id)}>X</button>
+            <button className = "btn red" onClick={() => props.removeUnit(props.id)}>X</button>
             </td>
         </tr>
     )
@@ -26,7 +26,7 @@ function UnitButton(props) {
             <button className = "waves-effect waves-light btn blue" onClick={() => props.changeActive(props.id)}>Active</button>
             </td>
             <td>
-            <button className = "btn"onClick={() => props.removeUnit(props.id)}>X</button>
+            <button className = "btn red"onClick={() => props.removeUnit(props.id)}>X</button>
             </td>
         </tr>
         )
