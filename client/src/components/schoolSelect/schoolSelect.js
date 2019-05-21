10bde399cee87619cb04b589a91a6db2cc025b6a
jordan.werre <47542013+jwerre1@users.noreply.github.com>
2019-05-20 23:39:50 -0400

    readme started, bugfix with mw api

--- client/src/components/schoolSelect/schoolSelect.js
+++ client/src/components/schoolSelect/schoolSelect.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { Select } from 'react-materialize';
 
 import ReactMaterialSelect from 'react-material-select'
 import 'react-material-select/lib/css/reactMaterialSelect.css'
@@ -8,7 +7,7 @@ function SchoolSelect(props) {
     return (
         <ReactMaterialSelect name="schoolNumber" label="Please Choose A School" onChange={props.handleInputChange.bind(this)}>
             <option dataValue="1">Reedy Creek Elementary</option>
-            <option dataValue="2">School 2</option>
+            <option dataValue="2">Koa Elementary</option>
         </ReactMaterialSelect>
 
     )
